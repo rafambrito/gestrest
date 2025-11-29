ab533c3430abbb41127ebf948afbeef3f2acfca0
Vithor Llaguno Veronese <vithor.veronese@outlook.com>
2025-11-29 16:40:11 -0300

    feat: Implement password hashing

--- src/main/java/br/com/gestrest/services/UserService.java
+++ src/main/java/br/com/gestrest/services/UserService.java
@@ -3,6 +3,7 @@ package br.com.gestrest.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -11,10 +12,13 @@ import br.com.gestrest.repositories.UserRepository;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> findAllUsers(int page, int size) {
@@ -27,11 +31,15 @@ public class UserService {
     }
 
     public void saveUser(User newUser) {
+        newUser.setSenha(passwordEncoder.encode(newUser.getSenha()));
+
         var save = this.userRepository.save(newUser);
         Assert.state(save == 1, "Error saving user " + newUser.getNome());
     }
 
     public void updateUser(User oldUser, Long id) {
+        oldUser.setSenha(passwordEncoder.encode(oldUser.getSenha()));
+
         var update = this.userRepository.update(oldUser, id);
         if (update == 0) {
             throw new RuntimeException("User not found");
